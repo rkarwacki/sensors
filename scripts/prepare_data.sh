77e7f309d2e88b1840d50e398e45f49d3b791c1b
Radek <karwacki.radoslaw@gmail.com>
2024-02-15 20:27:33 +0100

    fix filename

--- scripts/prepare_data.sh
+++ scripts/prepare_data.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-tail -n 720 ~/projects/sensors/data/sensors.csv > last_12_hours.csv && python csv_to_json.py last_12_hours.csv ../data/last_12_hours.js
+tail -n 720 ~/projects/sensors/data/sensors.csv > last_12_hours.csv && python csv_to_json.py last_12_hours.csv ../data/data_12h.js
 rm last_12_hours.csv
