a255a8d66e78c7d6da01c69db3f832c9519d0e4a
Radosław Karwacki <karwacki.radoslaw@gmail.com>
2024-02-15 20:20:06 +0100

    fix publishing data

--- scripts/prepare_data.sh
+++ scripts/prepare_data.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-tail -n 720 ~/projects/sensors/data/sensors.csv > last_12_hours.csv && python csv_to_json.py last_12_hours.csv last_12_hours.json
+tail -n 720 ~/projects/sensors/data/sensors.csv > last_12_hours.csv && python csv_to_json.py last_12_hours.csv ../data/last_12_hours.js
+rm last_12_hours.csv
