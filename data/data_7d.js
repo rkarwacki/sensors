3155dcfc0fdfc81b788605abd08c955e5db96cb5
pi-local <pi@radek.local>
2024-08-30 03:32:02 +0200

    Update: 2024-08-30 03:32:02

--- data/data_7d.js
+++ data/data_7d.js
@@ -1,15 +1,5 @@
 let data_7d = [
   {
-    "timestamp": "2024-08-23 03:02:02",
-    "averaged_temp": 24.1,
-    "humidity": 40.7
-  },
-  {
-    "timestamp": "2024-08-23 03:17:02",
-    "averaged_temp": 24.0,
-    "humidity": 41.0
-  },
-  {
     "timestamp": "2024-08-23 03:32:02",
     "averaged_temp": 24.0,
     "humidity": 41.0
@@ -3358,5 +3348,15 @@ let data_7d = [
     "timestamp": "2024-08-30 02:47:01",
     "averaged_temp": 24.0,
     "humidity": 45.3
+  },
+  {
+    "timestamp": "2024-08-30 03:02:02",
+    "averaged_temp": 24.0,
+    "humidity": 45.4
+  },
+  {
+    "timestamp": "2024-08-30 03:17:02",
+    "averaged_temp": 24.1,
+    "humidity": 45.4
   }
 ]
